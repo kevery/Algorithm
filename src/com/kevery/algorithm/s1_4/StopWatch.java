8485d167539a3d72a305362505068f0ed8b257b6
kever <416314413@163.com>
2016-12-21 23:24:10 +0800

    v1
    (cherry picked from commit 1b98795)

--- src/com/kevery/algorithm/s1_4/StopWatch.java
+++ src/com/kevery/algorithm/s1_4/StopWatch.java
@@ -23,5 +23,6 @@ public class StopWatch {
         System.out.println(1);
         double time = stopWatch.elapsedTime();
         System.out.println(time);
+        System.out.println("dev");
     }
 }
